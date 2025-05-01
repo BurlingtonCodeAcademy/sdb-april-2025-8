bfaf1c6ca5ac0d44150878592678ea7ed5b46015
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2025-05-01 17:30:00 -0500

    did some calculating

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Hello, world!");
+// Virtual Vending Machine
+// Using JavaScript variables and conditionals
+
+// Step 1: Define variables
+let userMoney = 1.00; // User's money
+let item = "soda";
+let price = 2.00;
+
+// Step 2: Use a conditional statment
+if (userMoney >= price) {
+    // some code here
+    let change = userMoney - price;
+    console.log("You bought " + item + " for $" + price + ".");
+    console.log("Your change is $" + change + ".");
+} else {
+    // some other code here
+    console.log("Insufficient funds. Please add more money.");
+}
